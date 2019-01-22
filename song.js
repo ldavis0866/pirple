798437719513718aa0e18c675006287b83257e65
Lonnie Davis <ldavis0866@gmail.com>
2019-01-22 17:15:28 -0600

    homework1

---
+++ song.js
@@ -0,0 +1,27 @@
+/***   HomeWork 1 Assignment  ***/
+
+// Variables for favorite song
+
+var name = "Shallow";
+var artist = "Lady Gaga";
+var movie = "A Star is Born";
+var release = 2018;
+var genre = ["Country Music", "Folk Pop"];
+var nominations = ["Grammy Award for Record of the Year", "Grammy Award for Song of the Year", "Academy Award for Best Original Song"];
+var awardsObject {
+    awards: ["Golden Globe Award for Best Original Song", "Critics Choice Movie Award for Best Song"],
+var producers: ["Lady Gaga", "Benjamin Rice"],
+var isAwesome: true,
+};
+
+// Print values to console
+console.log(name);
+console.log(artist);
+console.log(movie);
+console.log(release);
+console.log(genre);
+console.log(nominations);
+console.log(awardsObject);
+console.log(producers);
+console.log(isAwesome);
+
